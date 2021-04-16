41bc520e1b3978e794cc2dfc46fccb9376beb35f
Florin Dumitru <dumitru.florin@outlook.com>
2021-04-16 09:22:13 +0300

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const fileName = './package.json';
+const file = require(fileName);
+    
+file.build = file.build + 1;
+    
+fs.writeFile(fileName, JSON.stringify(file, null, 4), function writeJSON(err) {
+  if (err) return console.log(err);
+});
